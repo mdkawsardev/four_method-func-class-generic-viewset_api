253d6dbf82ecee5e72c8bebd3ab533f3296378cf
mdkawsar <info.mdkawsardev@gmail.com>
2026-05-06 15:12:03 +0600

    Models created for four views

--- funcView/models.py
+++ funcView/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Students(models.Model):
+    Name = models.CharField(max_length=100)
+    Age = models.IntegerField()
+    Department = models.CharField(max_length=100)
+    Created_At = models.DateTimeField(auto_now_add=True)
+
+class Citizen(Students):
+    National_Id = models.IntegerField()
+
+class Animals(models.Model):
+    Name = models.CharField(max_length=100)
+
+class Fruits(models.Model):
+    Name = models.CharField(max_length=100)
