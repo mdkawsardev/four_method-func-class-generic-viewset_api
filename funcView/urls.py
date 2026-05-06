68de6453e874a72a646818ae9aaf9228396bf3bc
mdkawsar <info.mdkawsardev@gmail.com>
2026-05-06 16:46:47 +0600

    In function based view, I applied GET, POST, Retrieve, PUT, and DELETE method

--- funcView/urls.py
+++ funcView/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from funcView.views import *
 urlpatterns = [
-    path('data/', listData, name='data')
+    path('data/', listData, name='data'),
+    path('data/<int:pk>/', UpdateRetrieveDelete, name='data'),
 ]
--- funcView/views.py
+++ funcView/views.py
@@ -5,7 +5,7 @@ from funcView.serializers import StudentsSerializer
 from funcView.models import Students
 
 @api_view(["GET", "POST"])
-def listData(request):
+def listData(request, format=None):
     if request.method == "GET":
         obj = Students.objects.all()
         serializer = StudentsSerializer(obj, many=True)
@@ -16,3 +16,20 @@ def listData(request):
             serializer.save()
             return Response({"New data added": serializer.data})
 
+@api_view(["GET", "PUT", "DELETE"])
+def UpdateRetrieveDelete(request, pk):
+    try:
+        queryset = Students.objects.get(id=pk)
+    except Students.DoesNotExist:
+        return Response("Something went wrong!")
+    if request.method == "GET":
+        serializer = StudentsSerializer(queryset)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = StudentsSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+    elif request.method == "DELETE":
+        queryset.delete()
+        return Response({"Data deletion": "Success"})
