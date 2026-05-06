e9703d91125284b8db7559ec50f38ba8a3af82da
mdkawsar <info.mdkawsardev@gmail.com>
2026-05-06 16:19:15 +0600

    In function based view, I applied GET and POST method. It worked fine

--- funcView/views.py
+++ funcView/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from funcView.serializers import StudentsSerializer
+from funcView.models import Students
 
-@api_view(["GET", "POST", "PUT", "DELETE"])
+@api_view(["GET", "POST"])
 def listData(request):
-    pass
+    if request.method == "GET":
+        obj = Students.objects.all()
+        serializer = StudentsSerializer(obj, many=True)
+        return Response({"Message": "OK", "Data": serializer.data})
+    elif request.method == "POST":
+        serializer = StudentsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"New data added": serializer.data})
+
